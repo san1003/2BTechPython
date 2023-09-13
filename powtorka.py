1f0d7043c9fb8a8058049fc873dfecba1a014cd6
san1003 <114583306+san1003@users.noreply.github.com>
2023-09-13 12:13:23 +0000

    zrobiono zadanie od 1 do 6

---
+++ powtorka.py
@@ -0,0 +1,29 @@
+# zad1
+# # a = int(input())
+# b = int(input())
+# print((a**2)+(b**2))
+
+#zad2
+# a = int(input())
+# b = int(input())
+# print((a+b)**2)
+
+#zad3
+# a = int(input())
+# b = int(input())
+# print((a-b)**3)
+
+#zad4
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(a*b*c)
+
+#zad5
+# a = int(input())
+# b = int(input())
+# print(2*(a+b)/5)
+
+#zad6
+# brutto = int(input())
+# print(brutto/1.23)
