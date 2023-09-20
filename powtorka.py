d10279eb275ce189aca47a43b5fea7ee355813b7
san1003 <114583306+san1003@users.noreply.github.com>
2023-09-20 12:13:04 +0000

    def

--- powtorka.py
+++ powtorka.py
@@ -1,5 +1,5 @@
 # zad1
-# # a = int(input())
+# a = int(input())
 # b = int(input())
 # print((a**2)+(b**2))
 
@@ -27,3 +27,42 @@
 #zad6
 # brutto = int(input())
 # print(brutto/1.23)
+
+#zad7
+# a = int(input())
+# b = int(input())
+# print(a%b)
+
+# def sumaKwadratow(a,b):
+#   return (a**2)+(b**2)
+
+# def dodawanieliczbdokwadratow(a,b):
+#   return (a+b)**2
+
+# def szescianroznicy(a,b):
+#   return (a-b)**3
+
+# def iloczyn3liczb(a,b,c):
+#   return a*b*c
+
+# def cos(a,b):
+#   return 2*(a+b)/5
+
+# def brutto(a):
+#  return a/1.23
+
+# a = int(input())
+# def podzielny3(a):
+#   return a%3==0
+# print(podzielny3(a))  
+
+b = int(input())
+def cyfrpo17(b):
+  return b>100 and b<1000 and b%17==0
+print(cyfrpo17(b))  
+  
+
+
+  
+
+
